a0b3856f8f65d37635c386283099d052e0429a10
kritikaparmar-programmer <kprogrammer07@gmail.com>
2020-07-16 13:22:52 +0530

    Learning Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+//console.log('Hello World!!')
+function toggleDarkLight(){
+	var body = document.getElementById("body");
+	var currentClass = body.className;
+	body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
